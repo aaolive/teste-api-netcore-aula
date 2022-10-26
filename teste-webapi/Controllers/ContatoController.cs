1fae0d3a4ba0698768039af7c877e34e5011393e
Anderson <anderson.assis@outlook.com>
2022-10-25 22:28:25 -0300

    Add metodo para atualizar deletar e buscar contato

--- teste-webapi/Controllers/ContatoController.cs
+++ teste-webapi/Controllers/ContatoController.cs
@@ -25,5 +25,65 @@ namespace teste_webapi.Controllers
             _context.SaveChanges();
             return Ok();
         }
+
+        // Busca contato por id
+        [HttpGet("{id}")] // recebe na rota um parametro id
+        public IActionResult ObterPorID(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            // testa se contato é nulo
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contato);
+        }
+
+        // buca contato por nome
+        [HttpGet("ObterPorNome")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            // faz pesquisa pelo nome
+            var contatos = _context.Contatos.Where(c => c.Nome.Contains(nome));
+            return Ok(contatos);
+        }
+
+        // metodo para atualizar contato
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contatoModificado)
+        {
+            var contatoNoBancoDeDados = _context.Contatos.Find(id);
+            if (contatoNoBancoDeDados == null)
+            {
+                return NotFound();
+            }
+
+            contatoNoBancoDeDados.Nome = contatoModificado.Nome;
+            contatoNoBancoDeDados.Telefone = contatoModificado.Telefone;
+            contatoNoBancoDeDados.Ativo = contatoModificado.Ativo;
+
+            _context.Contatos.Update(contatoNoBancoDeDados);
+            _context.SaveChanges();// salva alteracao no banco
+
+            return Ok(contatoNoBancoDeDados);
+        }
+
+        // metodo para deletar
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var contatoNoBancoDeDados = _context.Contatos.Find(id);
+            if (contatoNoBancoDeDados == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contatos.Remove(contatoNoBancoDeDados);
+            _context.SaveChanges(); // salva alteracao no banco
+
+            return NoContent();
+        }
     }
 }
